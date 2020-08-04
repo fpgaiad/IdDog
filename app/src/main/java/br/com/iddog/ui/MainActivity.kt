aa160825e665bb5b49eccdd0ff78e886794ef64b
Felipe Gaiad <fpgaiad@gmail.com>
2020-08-03 22:15:19 -0300

    Change MainActivity package from iddog to iddog/ui

--- app/src/main/java/br/com/iddog/ui/MainActivity.kt
+++ app/src/main/java/br/com/iddog/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package br.com.iddog
+package br.com.iddog.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.iddog.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
